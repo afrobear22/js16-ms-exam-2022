cb6008ca1db7d4e59b573b37648325555f71bc66
afrobear22 <jakimovski22@gmail.com>
2022-04-06 21:11:38 +0200

    Portable computer

---
+++ api/handlers/poc.js
@@ -0,0 +1,48 @@
+const getAll = async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send('Internal Server Error');
+    }
+}
+const create = async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send('Internal Server Error');
+    }
+}
+const getOne = async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send('Internal Server Error');
+    }
+}
+const update = async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send('Internal Server Error');
+    }
+}
+const updatePartial = async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send('Internal Server Error');
+    }
+}
+const remove = async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send('Internal Server Error');
+    }
+}
---
+++ api/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const poc = require('./handlers/poc');
+
+const api = express();
+
+api.use(express.json());
+
+api.get('/api/portable-computer', poc.getAll);
+api.post('/api/portable-computer', poc.create);
+api.get('/api/portable-computer/:id', poc.getOne);
+api.put('/api/portable-computer/:id', poc.update);
+api.patch('/api/portable-computer/:id', poc.updatePartial);
+api.delete('/api/portable-computer/:id', poc.remove);
+
+api.listen(config.get('service').port, err => {
+    if (err) return console.log(err);
+    return console.log(`Server started on port ${config.get('service').port}`)
+})
